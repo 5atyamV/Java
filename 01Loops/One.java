3db5c9b1ce673e924f8c894870829b6f16458ff3
SATYAM <133624894+5atyamV@users.noreply.github.com>
2024-05-26 18:00:05 +0000

    done

--- 01Loops/One.java
+++ 01Loops/One.java
@@ -71,6 +71,73 @@ public class One {
 
 }
 
+public static void prymid2(int n){
+    for(int i =0; i<n; i++){
+        
+        for(int j=0; j<i; j++){
+            System.out.print("_");
+        }
+
+        for(int j=0; j<2*n-(2*i+1); j++){
+            System.out.print("*");
+        }
+        for(int j=0; j<i; j++){
+            System.out.print("_");
+        }
+
+        System.out.println();
+}
+
+}
+
+public static void prymid3(int n){
+    for(int i =0; i<n; i++){
+            
+        for(int j=0; j<n-i-1; j++){
+            System.out.print("_");
+        }
+
+        for(int j=0; j<2*i+1; j++){
+            System.out.print("*");
+        }
+        for(int j=0; j<n-i-1; j++){
+            System.out.print("_");
+        }
+
+        System.out.println();
+}
+
+///
+for(int i =0; i<n; i++){
+        
+    for(int j=0; j<i; j++){
+        System.out.print("_");
+    }
+
+    for(int j=0; j<2*n-(2*i+1); j++){
+        System.out.print("*");
+    }
+    for(int j=0; j<i; j++){
+        System.out.print("_");
+    }
+
+    System.out.println();
+}
+
+
+}
+public static void pyrmid4(int n){
+    for(int i =1; i<=2*n-1; i++){
+        int stars = i;
+        if (i > n) stars = 2*n - i;
+        for(int j=1; j<=stars; j++){
+            System.out.print("*");
+
+        }
+        System.out.println();
+    }
+
+}
 
 
 
@@ -82,6 +149,9 @@ public class One {
        //NumberTriangleTwo(s);
       // traiangle(s);
       //prymid(s);
+      //prymid2(s);
+      prymid3(s);
+      pyrmid4(s);
        
 
        
