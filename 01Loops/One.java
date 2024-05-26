57cab130d83fa1cb61d9b79d380c8e2a1e30a6b1
SATYAM <133624894+5atyamV@users.noreply.github.com>
2024-05-26 19:09:13 +0000

    done

--- 01Loops/One.java
+++ 01Loops/One.java
@@ -140,6 +140,56 @@ public static void pyrmid4(int n){
 }
 
 
+public static void zeroOne(int n){
+    int start = 1;
+    for(int i =0; i<n; i++){
+        if( i%2 == 0) start = 1;
+        else start = 0;
+        for(int j =0; j<i; j++){
+            System.out.print(start);
+            start = 1 - start;
+     
+        }
+        
+    System.out.println();
+    }
+
+}
+
+public static void patternnum(int n ){
+    int space = 2*(n-1);
+    for(int i =1; i<=n; i++){
+        //num
+        for(int j =1; j<=i; j++){
+        System.out.print(j);
+        }
+        //space
+        for(int j =1; j<=space; j++){
+            System.out.print(" ");
+            }
+
+        //num
+        for(int j =i; j>=1; j--){
+            System.out.print(j);
+            }
+
+            System.out.println();
+            space = space - 2;
+    }
+
+}
+
+public static void numprymid(int n ){
+    int num = 1;
+    for(int i =1; i<=n; i++){
+        //num
+        for(int j =1; j<=i; j++){
+        System.out.print("*");
+        num++;
+        }
+    }
+}
+
 
     public static void main(String[] args) {
         int s = 5;
@@ -150,10 +200,11 @@ public static void pyrmid4(int n){
       // traiangle(s);
       //prymid(s);
       //prymid2(s);
-      prymid3(s);
-      pyrmid4(s);
-       
-
+      //prymid3(s);
+      //pyrmid4(s);
+     // zeroOne(s);
+       //patternnum(s);
+       numprymid(s);
        
      
     }
